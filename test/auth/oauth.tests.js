5ce37c5f11314d2f38c7f17f95b38fd9f3e40e0d
Robert Groves <rwgdev@gmail.com>
2020-04-27 17:06:43 -0500

    Add test for empty connection.

--- test/auth/oauth.tests.js
+++ test/auth/oauth.tests.js
@@ -89,6 +89,13 @@ describe('OAuthAuthenticator', function() {
       expect(signIn).to.throw(ArgumentError, 'connection field is required');
     });
 
+    it('should not allow an empty connection', function() {
+      var auth = this.authenticator;
+      var signIn = auth.signIn.bind(auth, { connection: '' });
+
+      expect(signIn).to.throw(ArgumentError, 'connection field is required');
+    });
+
     it('should accept a callback', function(done) {
       this.authenticator.signIn(userData, done.bind(null, null));
     });
